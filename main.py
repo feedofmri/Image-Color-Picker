ca67abe2c39b1ebaca4b7661dec9ea6fee074ff7
Rubayet <77844439+feedofmri@users.noreply.github.com>
2024-03-09 01:10:00 +0600

    added hex option

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from colorthief import ColorThief
 import matplotlib.pyplot as plt
+import colorsys
 
 img = ColorThief('image.jpg')
 
@@ -9,3 +10,8 @@ palette = img.get_palette(color_count=6)
 
 plt.imshow([palette[i] for i in range(6)])
 plt.show()
+
+print("Color Palette:")
+for color in palette:
+    print(color)
+    print(f"#{color[0]:02x}{color[1]:02x}{color[2]:02x}")
